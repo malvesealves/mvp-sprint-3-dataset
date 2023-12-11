f065a11e12b48a14c61d8c17090bd24a66d443d7
Mateus Alves <m.alvesealves@gmail.com>
2023-12-11 12:58:10 -0300

    Insere arquivo de notebook no projeto

--- wine_classification_api/test_modelos.py
+++ wine_classification_api/test_modelos.py
@@ -2,8 +2,6 @@ from model.avaliador import Avaliador
 from model.carregador import Carregador
 from model.modelo import Model
 
-# To run: pytest -v test_modelos.py
-
 # Instanciação das Classes
 carregador = Carregador()
 modelo = Model()
@@ -20,16 +18,16 @@ dataset = carregador.carregar_dados(url_dados, colunas)
 X = dataset.iloc[:, 0:-1]
 Y = dataset.iloc[:, -1]
     
-# Método para testar modelo KNN a partir do arquivo correspondente
+# Método para testar modelo KNN padronizado a partir do arquivo correspondente
 def test_modelo_knn():
     # Importando modelo de KNN
-    knn_path = 'ml_model/winequality_knn.pkl'
+    knn_path = 'ml_model/winequality_knn_pdr.pkl'
     modelo_knn = modelo.carrega_modelo(knn_path)
 
     # Obtendo as métricas do KNN
     acuracia_knn = avaliador.avaliar(modelo_knn, X, Y)
     
-    # Testando as métricas do KNN
+    # Testando as métricas do KNN padronizado
     # Modifique as métricas de acordo com seus requisitos
     assert acuracia_knn >= 0.60
     
